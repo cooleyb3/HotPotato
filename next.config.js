ad42668e2819c6aa30146f56a8fef8361eee7571
I-Patience <irakozeptnc@gmail.com>
2025-08-26 08:32:46 +0200

    Remove redirect to allow direct access to farcaster.json manifest

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/YOUR_HOSTED_MANIFEST_ID',
-        permanent: false,
-      },
-    ];
-  },
   images: {
     domains: ['hotpotatogame.vercel.app'],
   },
-
 };
 
 module.exports = nextConfig;
